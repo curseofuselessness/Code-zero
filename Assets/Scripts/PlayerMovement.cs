32ffbc35d17bd9b68847cdad6f3808ea678c20ee
Кирилл <fantom_youtube@inbox.ru>
2025-10-06 00:50:09 +0300

    added simple physics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+//using static Unity.Cinemachine.InputAxisControllerBase<T>;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform Head, Body;         // объект Head
-    public Animator animator;
 
+    // Объекты  
+    public Transform Head, Body;       
+    public Animator animator;
+    public CharacterController controller;
+
+
+    // Движение игрока в пространстве
     public float walkSpeed = 5f;
     public float mouseSensitivity = 3f;
 
     private float pitch = 0f;      // вертикальный угол головы
     private float yaw = 0f;        // горизонтальный угол тела
+
+    // ФИЗИКА
+    private float gravity = 9.81f;
+    private float fallSpeed = 1f;
+
+    private void Start()
+    {
+        animator.applyRootMotion = false;
+    }
 
     void Update()
     {
@@ -21,19 +36,33 @@ public class PlayerMovement : MonoBehaviour
         pitch -= mouseY;
         pitch = Mathf.Clamp(pitch, -60f, 60f);
 
-        
 
-        Body.localRotation = Quaternion.Euler(0, yaw, 0);                 // вращение тела
+
+        //transform.rotation = Quaternion.Euler(0, yaw, 0);               // вращение тела
+        Body.localRotation = Quaternion.Euler(0, yaw, 0);
         Head.localRotation = Quaternion.Euler(pitch, 0, 0);              // вращение головы
 
         // Движение WASD
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical);
-        movement = Body.TransformDirection(movement);               // локальное направление
+        Vector3 input = new Vector3(horizontal, 0, vertical);
+        Vector3 move = Body.TransformDirection(input) * walkSpeed;
 
-        Body.position += movement * walkSpeed * Time.deltaTime;
+        if (controller.isGrounded)
+        {
+            fallSpeed = -1;
+        }
+        else
+        {
+            fallSpeed += gravity;
+        }
+
+        // Применение гравитации
+        move.y -= fallSpeed * Time.deltaTime;
+
+        // Перемещение с учётом коллизий
+        controller.Move(move * Time.deltaTime);
 
         animator.SetFloat("Speed", vertical);
     }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 survival-horror based on unity
 
-###[development plan(RU)](https://app.holst.so/share/b/b8e3c784-252d-4972-8e7c-2ae9aa5f765d)
+[development plan(RU)](https://app.holst.so/share/b/b8e3c784-252d-4972-8e7c-2ae9aa5f765d)
